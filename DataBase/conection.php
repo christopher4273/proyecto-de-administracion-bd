92eb94356b6b18d89330c2569e68a99c94b4bc6f
Enma123 <joseemiliogr5@gmail.com>
2021-09-06 12:42:37 -0600

    12:42

--- DataBase/conection.php
+++ DataBase/conection.php
@@ -1,10 +1,10 @@
 <?php
-$serverName = "localhost";
+$serverName = "localhost\proyecto-de-administracion-bd"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"marketplace", "UID"=>"user", "PWD"=>"password");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
-$connectionInfo = array( "Database"=>"marketplace");
-$Conn = sqlsrv_connect( $serverName, $connectionInfo);
-if($Conn) {
-     //echo "Conexión establecida.<br />";
+if( $conn ) {
+     echo "Conexión establecida.<br />";
 }else{
      echo "Conexión no se pudo establecer.<br />";
      die( print_r( sqlsrv_errors(), true));
