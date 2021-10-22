b2f57b11ff7f1f24f0bffe2a2d8379c447d58335
Enma123 <joseemiliogr5@gmail.com>
2021-10-21 20:53:56 -0600

    20:53

---
+++ Controllers/InvoiceController.php
@@ -0,0 +1,51 @@
+<?php
+//Se incluye el modelo donde conectará el controlador.
+require_once '../Models/Invoice.php';
+//require_once '../Models/Detalle.php';
+require_once '../DataBase/Conection.php';    
+class FacturaController{
+    private $factura;
+    private $detalle;
+    //Creación del modelo
+    public function __CONSTRUCT(){
+        $this->factura = new Factura();
+        //$this->detalle = new Detalle();
+    }
+    //Método que registrar.
+    public function save(){
+        if(isset($_POST['InvoiceController'])){
+        $pvd = new Factura();
+        //Captura de los datos del formulario (vista).
+        $pvd->id_factura = $_POST['id_factura'];
+        $pvd->fecha =  date("Y-m-d H:i:s");
+        $pvd->subtotal = 0;
+        $pvd->impuesto = 0;
+        $pvd->total = 0;
+        $pvd->cliente = $_POST['cliente'];
+        $pvd->vendedor = $_POST['vendedor'];
+        //Registro.
+        $this->factura->add($pvd);
+        echo '<script>window.open("../Views/NewInvoice.php","_self",null,true);</script>';
+    
+     }
+
+    }
+
+    /*public function calculate($id){
+        $pvd = new Factura();
+        $f=new Factura();
+        $array = $this->detalle->details($id);
+        $f = $this->factura->search($id);
+        $sub=0;
+        $det=0;
+        foreach($array as $detail){
+            $det = $detail->subtotal;
+            $sub = $sub+$det;
+        }
+        $f->subtotal = $sub;
+        $f->impuesto = $sub*0.13;
+        $f->total = $f->subtotal+$f->impuesto;
+        $pvd=$f;
+        $this->factura->update($pvd);
+    }*/
+}
--- DataBase/Stored procedures/sp_factura.sql
+++ DataBase/Stored procedures/sp_factura.sql
@@ -42,17 +42,3 @@ AS
 		WHERE id_factura = @id_factura
 	END
 --READ
-
-insert into tbcliente(id_cliente, nombre_completo, correo, numero_telefonico) values ('123','mario', 'mamama', 84848484);
-
-insert into tbusuario(id_usuario, nombre_completo, correo, telefono) values ('456','sonia', 'mamama', 84848484);
-
-EXEC createsp_factura 
-			 @id_factura = 16,
-			 @fecha = '02/02/2021',
-			 @subtotal = 75000,
-			 @impuesto = 1000,
-			 @total = 74000,
-			 @cliente = 123,
-			 @vendedor = 456
-go
