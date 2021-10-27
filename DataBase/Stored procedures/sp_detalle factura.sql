3c0c2b5300bb218438ee049b97ceb191bb59905f
Marvin Juarez Aguilar <68169947+R3IZA@users.noreply.github.com>
2021-10-26 21:49:03 -0600

    Update sp_detalle factura.sql

---
+++ DataBase/Stored procedures/sp_detalle factura.sql
@@ -0,0 +1,23 @@
+--CRUD DE PROCEDIMIENTOS DE ALMACENAMIENTO DE DETALLE DE USUARIO
+--CREATE
+CREATE PROCEDURE sp_DetalleFactura
+	@subtotal float,
+	@descuento float,
+	@cantidad int,
+	@factura int,
+	@producto int
+AS
+	BEGIN
+	IF((select stock from tbproducto where id_producto = @producto) >= @cantidad)
+		BEGIN
+		update tbproducto set stock=stock-@cantidad from tbproducto where id_producto = @producto
+		INSERT INTO tbdetallefactura (subtotal, descuento, cantidad, factura, producto) 
+		VALUES (@subtotal, @descuento, @cantidad, @factura, @producto)
+		return 0
+		END
+	ELSE
+		BEGIN
+		Print 'Las unidades existentes no son suficientes para realizar la venta.'
+		END
+	END
+GO
