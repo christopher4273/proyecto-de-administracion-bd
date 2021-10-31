b3929006ba0260f4b44a2ea9be54d62fade81680
felioxd <83518718+felioxd@users.noreply.github.com>
2021-10-30 21:42:30 -0600

    Update sp_producto.sql

--- DataBase/Stored procedures/sp_producto.sql
+++ DataBase/Stored procedures/sp_producto.sql
@@ -40,6 +40,41 @@ AS
         WHERE  (id_producto = @id_producto)
     END
 GO
+--UPDATE
+CREATE PROC csp_ProductoUpdate
+    @id_producto INT, 
+    @descripcion VARCHAR(50),
+    @stock INT,
+    @precio FLOAT,
+    @categoria INT
+
+AS 
+    BEGIN 
+        UPDATE tbproducto
+        SET  
+               descripcion = @descripcion,
+               stock = @stock,
+               precio = @precio,
+               categoria = @categoria
+			   FROM tbproducto as p inner join tbcategoria as c on p.categoria = c.id_categoria
+        WHERE  id_producto = @id_producto
+    END
+GO
+--UPDATE STOCK
+CREATE PROC csp_ProductoStockUpd
+    @id_producto INT, 
+    @stock INT
+
+AS 
+    BEGIN 
+        UPDATE tbproducto
+        SET  
+               
+               stock = @stock
+    
+			   FROM tbproducto WHERE  id_producto = @id_producto
+    END
+GO
 --DELETE
 CREATE PROC csp_ProductoDelete 
     @id_producto int
